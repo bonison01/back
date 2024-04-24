10d0d64b844a727467b51d99c57bfd986d7b6f77
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-24 14:50:32 +0530

    Update abouts-main.tsx

--- back/app/(marketing)/bill/_components/abouts-main.tsx
+++ back/app/(marketing)/bill/_components/abouts-main.tsx
@@ -2,16 +2,3 @@ import React from "react";
 import Link from "next/link";
 import { Content } from "./content";
 
-export function AboutsMain() {
-  return (
-    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
-      <div className="border border-gray-400 rounded-lg p-4">
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-          <Link href="/delivery-rates">
-            <a>Contact 3</a>
-          </Link>
-        </button>
-      </div>
-    </div>
-  );
-}
