7b6163e17135d199cc8e834bc6b25e6006bdc828
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-24 14:53:34 +0530

    Update abouts-main.tsx

--- back/app/(marketing)/bill/_components/abouts-main.tsx
+++ back/app/(marketing)/bill/_components/abouts-main.tsx
@@ -6,8 +6,6 @@ export function AboutsMain() {
   return (
     <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
       
-      <div className="border border-gray-400 rounded-lg p-4">
-      </div>
     </div>
   );
 }
