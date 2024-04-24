669ceae7c017a119b88f98cef44d6883c6a1d479
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-24 14:52:12 +0530

    Update abouts-main.tsx

--- back/app/(marketing)/bill/_components/abouts-main.tsx
+++ back/app/(marketing)/bill/_components/abouts-main.tsx
@@ -2,3 +2,12 @@ import React from "react";
 import Link from "next/link";
 import { Content } from "./content";
 
+export function AboutsMain() {
+  return (
+    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
+      
+      <div className="border border-gray-400 rounded-lg p-4">
+      </div>
+    </div>
+  );
+}
