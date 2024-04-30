48693a6463ec56cfc6e0e912ec276b02b1e59e5d
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-30 22:41:33 +0530

    Update abouts-main.tsx

--- back/app/(marketing)/bill/_components/abouts-main.tsx
+++ back/app/(marketing)/bill/_components/abouts-main.tsx
@@ -1,15 +1,6 @@
 import React from "react";
-import Link from "next/link";  // Note: Link seems unused in this snippet.
+import Link from "next/link";
 
 export function AboutsMain() {
-  return (
-    <iframe
-      style={iframeStyle}
-      src="https://script.google.com/macros/s/AKfycbxy4qYFPBKtM-cmCgyxnJhMpLtnDIkr5w1NPL3lNpA2zz66LQ6tWSFA52dK2X3Fq4mmuw/exec"
-      allowFullScreen
-      width: '100vw',  // 100% of the viewport width
-      height: '100vh',  // 100% of the viewport height
-      border: 'none'  // Remove the border for a cleaner look
-    ></iframe>
-  );
-}
+  return <iframe src="https://script.google.com/macros/s/AKfycbxy4qYFPBKtM-cmCgyxnJhMpLtnDIkr5w1NPL3lNpA2zz66LQ6tWSFA52dK2X3Fq4mmuw/exec"></iframe> ;
+};
