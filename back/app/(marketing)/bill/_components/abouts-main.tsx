2a1e82a12339dd03e1f34ee084b8d9f43db3af2a
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-30 22:27:02 +0530

    Update abouts-main.tsx

--- back/app/(marketing)/bill/_components/abouts-main.tsx
+++ back/app/(marketing)/bill/_components/abouts-main.tsx
@@ -2,21 +2,8 @@ import React from "react";
 import Link from "next/link";
 
 export function AboutsMain() {
-  return (
-    <div className="h-screen flex justify-center items-center">
-      {/* Embedded Google Sheets iframe */}
-      <div className="w-full h-full">
-        {/* Apply styles to the iframe */}
-        <iframe
-          className="w-full h-full"
-          src="https://script.google.com/macros/s/AKfycbxy4qYFPBKtM-cmCgyxnJhMpLtnDIkr5w1NPL3lNpA2zz66LQ6tWSFA52dK2X3Fq4mmuw/exec"
-          title="Track your Order"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
-      </div>
-    </div>
-  );
+  return <iframe>src="https://script.google.com/macros/s/AKfycbxy4qYFPBKtM-cmCgyxnJhMpLtnDIkr5w1NPL3lNpA2zz66LQ6tWSFA52dK2X3Fq4mmuw/exec"
+         </iframe>};
 }
 
 export default AboutsMain;
