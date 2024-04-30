0c47b377da47af7eab3419b8b34d54951113b7a7
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-30 21:26:57 +0530

    Update abouts-main.tsx

--- back/app/(marketing)/bill/_components/abouts-main.tsx
+++ back/app/(marketing)/bill/_components/abouts-main.tsx
@@ -1,31 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 export default function AboutsMain() {
-  // Styles defined as JavaScript objects
-  const fullScreenStyle = {
-    width: '100vw',   // 100% of the viewport width
-    height: '100vh',  // 100% of the viewport height
-    margin: 0,
-    padding: 0,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-  };
-
-  const iframeStyle = {
-    width: '100%',    // 100% of the parent container
-    height: '100%'    // 100% of the parent container
-  };
-
   return (
-    <div style={fullScreenStyle}>
-      <iframe
-        style={iframeStyle}
-        src="https://script.google.com/macros/s/AKfycbyyQVK4hHNJZ6LngIYc889b1yCnzwuBfEl8xum4nDjZOGp1lHBXEbyts248HbZD6Eb28A/exec"
-        title="Track your Order"
-        frameBorder="0"
-        allowFullScreen
-      ></iframe>
+    <div className="container">
+      <div className="iframe-container">
+        <iframe
+          className="iframe-fullscreen"
+          src="https://script.google.com/macros/s/AKfycbyyQVK4hHNJZ6LngIYc889b1yCnzwuBfEl8xum4nDjZOGp1lHBXEbyts248HbZD6Eb28A/exec"
+          title="Track your Order"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
