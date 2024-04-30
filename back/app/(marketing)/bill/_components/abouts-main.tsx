138d58e07f228eec1a57879dea9019e92eb9ed79
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-30 21:54:03 +0530

    Update abouts-main.tsx

--- back/app/(marketing)/bill/_components/abouts-main.tsx
+++ back/app/(marketing)/bill/_components/abouts-main.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export function AboutsMain() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      {/* Embedded Google Sheets iframe */}
-      <div className="w-full h-full">
-        {/* Apply styles to the iframe */}
+    <div className="h-screen flex justify-center items-center overflow-x-auto">
+      {/* This container will allow horizontal scrolling */}
+      <div className="w-full h-full" style={{ minWidth: '300%' }}>
         <iframe
           className="w-full h-full"
+          style={{ width: '100%', height: '100%' }}
           src="https://script.google.com/macros/s/AKfycbyyQVK4hHNJZ6LngIYc889b1yCnzwuBfEl8xum4nDjZOGp1lHBXEbyts248HbZD6Eb28A/exec"
           title="Track your Order"
           frameBorder="0"
