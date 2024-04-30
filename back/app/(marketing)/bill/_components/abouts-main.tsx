3e2aca25d4d0c44fcfd4223efe239fb76d4d01c4
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-30 21:50:59 +0530

    Update abouts-main.tsx

--- back/app/(marketing)/bill/_components/abouts-main.tsx
+++ back/app/(marketing)/bill/_components/abouts-main.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export function AboutsMain() {
   return (
-    // Use the min-h-screen class to ensure that the div takes at least the full height of the viewport
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="h-screen flex justify-center items-center">
+      {/* Embedded Google Sheets iframe */}
       <div className="w-full h-full">
+        {/* Apply styles to the iframe */}
         <iframe
-          // Ensure that the iframe covers the full width and height of its container
           className="w-full h-full"
           src="https://script.google.com/macros/s/AKfycbyyQVK4hHNJZ6LngIYc889b1yCnzwuBfEl8xum4nDjZOGp1lHBXEbyts248HbZD6Eb28A/exec"
           title="Track your Order"
