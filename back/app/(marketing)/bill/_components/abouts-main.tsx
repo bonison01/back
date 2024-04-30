70423bcb6dbe430a4392deb476951220bbccf310
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-30 21:21:06 +0530

    Update abouts-main.tsx

--- back/app/(marketing)/bill/_components/abouts-main.tsx
+++ back/app/(marketing)/bill/_components/abouts-main.tsx
@@ -1,22 +1,31 @@
 import React from "react";
-import Link from "next/link";
 
-export function AboutsMain() {
+export default function AboutsMain() {
+  // Styles defined as JavaScript objects
+  const fullScreenStyle = {
+    width: '100vw',   // 100% of the viewport width
+    height: '100vh',  // 100% of the viewport height
+    margin: 0,
+    padding: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  };
+
+  const iframeStyle = {
+    width: '100%',    // 100% of the parent container
+    height: '100%'    // 100% of the parent container
+  };
+
   return (
-    <div className="h-screen flex justify-center items-center">
-      {/* Embedded Google Sheets iframe */}
-      <div className="w-full h-full">
-        {/* Apply styles to the iframe */}
-        <iframe
-          className="w-full h-full"
-          src="https://script.google.com/macros/s/AKfycbyyQVK4hHNJZ6LngIYc889b1yCnzwuBfEl8xum4nDjZOGp1lHBXEbyts248HbZD6Eb28A/exec"
-          title="Track your Order"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
-      </div>
+    <div style={fullScreenStyle}>
+      <iframe
+        style={iframeStyle}
+        src="https://script.google.com/macros/s/AKfycbyyQVK4hHNJZ6LngIYc889b1yCnzwuBfEl8xum4nDjZOGp1lHBXEbyts248HbZD6Eb28A/exec"
+        title="Track your Order"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
-
-export default AboutsMain;
