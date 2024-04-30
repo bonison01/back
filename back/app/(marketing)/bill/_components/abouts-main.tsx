b82e0128099f18911f35a18521433fdfab4da17e
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-30 22:03:26 +0530

    Update abouts-main.tsx

--- back/app/(marketing)/bill/_components/abouts-main.tsx
+++ back/app/(marketing)/bill/_components/abouts-main.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 
 export function AboutsMain() {
   return (
-      <div>
+    <div className="h-screen flex justify-center items-center">
+      {/* Embedded Google Sheets iframe */}
+      <div className="w-full h-full">
+        {/* Apply styles to the iframe */}
         <iframe
           className="w-full h-full"
-          style={{ width: '400%', height: '100%' }}
           src="https://script.google.com/macros/s/AKfycbyyQVK4hHNJZ6LngIYc889b1yCnzwuBfEl8xum4nDjZOGp1lHBXEbyts248HbZD6Eb28A/exec"
           title="Track your Order"
-          frameBorder="0"
+          frameBorder="1"
           allowFullScreen
         ></iframe>
       </div>
     </div>
   );
 }
-
-export default AboutsMain;
